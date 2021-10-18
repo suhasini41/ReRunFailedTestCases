6bdeae359de7c956fa1b594b5939dfba4ee0a65a
suhasini41 <suhasini.mb@gmail.com>
2021-10-17 22:12:39 -0500

    committed

---
+++ ReRunFailedTestCases/src/main/java/rerun/RetryClass.java
@@ -0,0 +1,21 @@
+package rerun;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryClass implements IRetryAnalyzer 
+{
+	int count = 0;
+	int max = 3;
+
+	@Override
+	public boolean retry(ITestResult result) {
+		// TODO Auto-generated method stub
+		if(count<max) {
+			count++;
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ ReRunFailedTestCases/src/test/java/tests/TestClass.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass
+{
+	@Test
+	public void testCase1()
+	{
+		Assert.assertTrue(false);
+	}
+
+}
